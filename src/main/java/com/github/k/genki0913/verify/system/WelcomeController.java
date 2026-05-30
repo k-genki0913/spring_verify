bdf1e35337c84f725a4c52090ee5f6a23c723e8e
k.genki0913 <k.genki0913@icloud.com>
2026-05-30 13:03:16 +0000

    最小のController、Template構成を追加

---
+++ src/main/java/com/github/k/genki0913/verify/system/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.github.k.genki0913.verify.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "welcome";
+    }
+}
