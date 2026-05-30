157024bd18a76ab5f46fd858f9370e71d78786e0
k.genki0913 <k.genki0913@icloud.com>
2026-05-30 15:57:36 +0900

    環境構築

---
+++ src/main/java/com/github/k/genki0913/verify/VerifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.k.genki0913.verify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/k/genki0913/verify/VerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.k.genki0913.verify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
