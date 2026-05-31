171c609bd19d5b476f6f73ca140a57f097a1c5a7
k.genki0913 <k.genki0913@icloud.com>
2026-05-31 05:45:55 +0000

    単体テストを追加

--- src/main/java/com/github/k/genki0913/verify/system/WelcomeController.java
+++ src/main/java/com/github/k/genki0913/verify/system/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 
     @GetMapping("/")
-    public String welcome() {
+    public String init() {
         return "welcome";
     }
 }
---
+++ src/test/java/com/github/k/genki0913/verify/system/WelcomeControllerTests.java
@@ -0,0 +1,48 @@
+package com.github.k.genki0913.verify.system;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(WelcomeController.class)
+public class WelcomeControllerTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Nested
+    @DisplayName("GET / (ホーム画面にアクセス)")
+    class Init {
+
+        @Test
+        @DisplayName("リクエストパラメータがない場合、ステータス200、View名がWelcome、HTML内にWelComeが含まれたレスポンスを返すこと")
+        void init_nonParam_return200AndWelComeView() throws Exception {
+            mockMvc.perform(
+                    get("/"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("welcome"))
+                    .andExpect(content().string(containsString("WelCome")));
+        }
+
+        @Test
+        @DisplayName("リクエストパラメータがある場合、ステータス200、View名がWelcome、HTML内にWelComeが含まれたレスポンスを返すこと")
+        void init_existParam_return200AndWelComeVIew() throws Exception {
+            mockMvc.perform(
+                    get("/")
+                            .param("test_key", "test_value"))
+                    .andExpect(status().isOk())
+                    .andExpect(view().name("welcome"))
+                    .andExpect(content().string(containsString("WelCome")));
+            ;
+        }
+    }
+}
