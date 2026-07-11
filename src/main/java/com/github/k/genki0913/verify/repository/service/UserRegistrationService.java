0d6b19bf1e3ebd431d8d7b97f06d151ad9c6c481
k.genki0913 <k.genki0913@icloud.com>
2026-07-11 05:46:04 +0000

    registerメソッドのjavadoc追加、ガード節の追加

--- src/main/java/com/github/k/genki0913/verify/repository/service/UserRegistrationService.java
+++ src/main/java/com/github/k/genki0913/verify/repository/service/UserRegistrationService.java
@@ -2,6 +2,7 @@ package com.github.k.genki0913.verify.repository.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import com.github.k.genki0913.verify.domain.User;
 import com.github.k.genki0913.verify.repository.jpa.UserRepository;
@@ -16,7 +17,27 @@ public class UserRegistrationService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * ユーザーの新規登録を行います。
+     * <p>
+     * 指定された {@link User} エンティティをデータベースへ保存します。
+     * </p>
+     * <p>
+     * <b>トランザクション管理:</b>
+     * <ul>
+     * <li>本メソッドは {@code @Transactional} によりトランザクション境界として管理されます。</li>
+     * <li>処理中に例外が発生した場合は、自動的にロールバックされます。</li>
+     * </ul>
+     * </p>
+     *
+     * @param user
+     *                 登録対象のユーザーエンティティ
+     * @return 保存されたユーザーエンティティ（ID等が自動採番された状態）
+     */
     public User register(User user) {
+
+        Assert.isNull(user.getId(), "新規登録時にはIDはnullである必要があります");
+
         return this.userRepository.save(user);
     }
 }
