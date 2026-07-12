69c2e6be25423c297dce91a343f118e0235a3aba
k.genki0913 <k.genki0913@icloud.com>
2026-07-12 06:59:29 +0000

    単体テストコードを追加

---
+++ src/test/java/com/github/k/genki0913/verify/common/validation/UniqueEmailValidatorTests.java
@@ -0,0 +1,59 @@
+package com.github.k.genki0913.verify.common.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.BeanPropertyBindingResult;
+
+import com.github.k.genki0913.verify.repository.form.UserRegistForm;
+import com.github.k.genki0913.verify.repository.service.UserRegistrationService;
+
+@ExtendWith(MockitoExtension.class)
+public class UniqueEmailValidatorTests {
+
+    @Mock
+    private UserRegistrationService userRegistrationService;
+
+    @InjectMocks
+    private UniqueEmailValidator uniqueEmailValidator;
+
+    @Test
+    @DisplayName("メールアドレスが重複している場合、エラーが登録されること")
+    void givenDuplicateEmail_whenValidate_thenReturnEmailFieldError() {
+        UserRegistForm form = new UserRegistForm();
+        form.setEmail("duplicatedEmail@example.com");
+        BeanPropertyBindingResult errors = new BeanPropertyBindingResult(form, "userRegistForm");
+
+        doReturn(true).when(userRegistrationService).isEmailRegistered("duplicatedEmail@example.com");
+
+        uniqueEmailValidator.validate(form, errors);
+
+        assertThat(errors.hasErrors()).isTrue();
+        assertThat(errors.getFieldError("email").getCode()).isEqualTo("common.email.duplicate");
+        verify(userRegistrationService, times(1)).isEmailRegistered("duplicatedEmail@example.com");
+    }
+
+    @Test
+    @DisplayName("メールアドレスが重複していない場合、エラーが登録さないこと")
+    void givenUniqueEmail_whenValidate_thenReturnNonError() {
+        UserRegistForm form = new UserRegistForm();
+        form.setEmail("uniqueEmail@example.com");
+        BeanPropertyBindingResult errors = new BeanPropertyBindingResult(form, "userRegistForm");
+
+        doReturn(false).when(userRegistrationService).isEmailRegistered("uniqueEmail@example.com");
+
+        uniqueEmailValidator.validate(form, errors);
+
+        assertThat(errors.hasErrors()).isFalse();
+        verify(userRegistrationService, times(1)).isEmailRegistered("uniqueEmail@example.com");
+    }
+}
