0a4600ab0c0c07747371158f220d40126cdfbcfa
k.genki0913 <k.genki0913@icloud.com>
2026-07-13 13:23:46 +0000

    未使用importを削除

--- src/test/java/com/github/k/genki0913/verify/common/validation/UniqueEmailValidatorTests.java
+++ src/test/java/com/github/k/genki0913/verify/common/validation/UniqueEmailValidatorTests.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
