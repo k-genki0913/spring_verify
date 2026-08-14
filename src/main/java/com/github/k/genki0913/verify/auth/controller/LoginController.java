4a5cb9809a7310961539df20cef0911c2b5fa785
k.genki0913 <k.genki0913@icloud.com>
2026-08-14 04:35:15 +0000

    loginパッケージをauthパッケージに変更

--- src/main/java/com/github/k/genki0913/verify/auth/controller/LoginController.java
+++ src/main/java/com/github/k/genki0913/verify/auth/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.github.k.genki0913.verify.login;
+package com.github.k.genki0913.verify.auth.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
