da56f5f9b392efa1402bd0acd7fd444686168cb4
k.genki0913 <k.genki0913@icloud.com>
2026-08-15 07:30:03 +0000

    DBから認証データを返却するサービス部分を追加

---
+++ src/main/java/com/github/k/genki0913/verify/auth/AccountUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.github.k.genki0913.verify.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountUserDetailsService implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    public AccountUserDetailsService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Account account = this.accountRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new AccountUserDetails(account);
+    }
+}
