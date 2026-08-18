f8adcdfeb7e3fbfebcbfb5fa0309f72af5fdd588
k.genki0913 <k.genki0913@icloud.com>
2026-08-18 14:11:29 +0000

    Jpaの監査設定をJpa用コンフィグファイルへ切り出し

--- src/main/java/com/github/k/genki0913/verify/VerifyApplication.java
+++ src/main/java/com/github/k/genki0913/verify/VerifyApplication.java
@@ -2,9 +2,7 @@ package com.github.k.genki0913.verify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 public class VerifyApplication {
 
---
+++ src/main/java/com/github/k/genki0913/verify/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.github.k.genki0913.verify.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class JpaConfig {
+}
