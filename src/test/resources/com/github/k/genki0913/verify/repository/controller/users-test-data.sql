b0c688140e19a6eb7a8e1db36838cf1d235b6c40
k.genki0913 <k.genki0913@icloud.com>
2026-08-21 15:51:41 +0000

    既存テストデータからIDを削除

--- src/test/resources/com/github/k/genki0913/verify/repository/controller/users-test-data.sql
+++ src/test/resources/com/github/k/genki0913/verify/repository/controller/users-test-data.sql
@@ -2,6 +2,6 @@
 DELETE FROM users;
 
 -- ユーザーテーブルにテストデータを挿入
-INSERT INTO users (id, name, email) VALUES (1, '山田太郎', 'taro@example.com');
-INSERT INTO users (id, name, email) VALUES (2, '山田花子', 'hanako@example.com');
-INSERT INTO users (id, name, email) VALUES (3, '佐藤次郎', 'jiro@example.com');
+INSERT INTO users (name, email) VALUES ('山田太郎', 'taro@example.com');
+INSERT INTO users (name, email) VALUES ('山田花子', 'hanako@example.com');
+INSERT INTO users (name, email) VALUES ('佐藤次郎', 'jiro@example.com');
