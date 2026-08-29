2a64d65b06223ec234602e5440a949553bacefb4
k.genki0913 <k.genki0913@icloud.com>
2026-08-29 08:32:44 +0000

    非チェック例外のスロー宣言を削除

--- src/main/java/com/github/k/genki0913/verify/auth/AccountUserDetailsService.java
+++ src/main/java/com/github/k/genki0913/verify/auth/AccountUserDetailsService.java
@@ -15,7 +15,7 @@ public class AccountUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         Account account = this.accountRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
