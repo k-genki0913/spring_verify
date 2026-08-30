c539129b6fa76694e2f2d0472dbba8a5d915cd63
k.genki0913 <k.genki0913@icloud.com>
2026-08-30 04:13:17 +0000

    変更されない値のためfinalを付加

--- src/main/java/com/github/k/genki0913/verify/config/AppValidationProperties.java
+++ src/main/java/com/github/k/genki0913/verify/config/AppValidationProperties.java
@@ -11,21 +11,21 @@ public class AppValidationProperties {
      * * GETバリデーション時に、メッセージから項目名を抽出するためのクエリマーカー。
      * デフォルト値として "?item.name=" を設定しておく。
      */
-    private String queryParamItemName = "?item.name=";
+    private final String queryParamItemName = "?item.name=";
 
     public String getQueryParamItemName() {
         return queryParamItemName;
     }
 
     /** プロパティファイル側で項目名を置換するための変数名テンプレート */
-    private String placeholder = "{item.name}";
+    private final String placeholder = "{item.name}";
 
     public String getPlaceholder() {
         return this.placeholder;
     }
 
     /** エラーメッセージをmodelに設定するためのattributeName */
-    private String errorAttributeName = "errors";
+    private final String errorAttributeName = "errors";
 
     public String getErrorAttributeName() {
         return this.errorAttributeName;
