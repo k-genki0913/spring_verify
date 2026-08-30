1c79b1682a54bca0d8d8d869d1387f73c4512202
k.genki0913 <k.genki0913@icloud.com>
2026-08-30 04:48:29 +0000

    View名用の定数をインターフェースからクラスへ変更

---
+++ src/main/java/com/github/k/genki0913/verify/validation/constant/ViewConst.java
@@ -0,0 +1,15 @@
+package com.github.k.genki0913.verify.validation.constant;
+
+/**
+ * Validationパッケージ専用のビュー名定数定義。
+ */
+public final class ViewConst {
+
+    public static final String SINGLE = "validation/single";
+
+    public static final String SINGLE_FORM = "validation/single-form";
+
+    public static final String CORRELATION_FORM = "validation/correlation-form";
+
+    public static final String PERIOD_SEARCH_FORM = "validation/period-search-form";
+}
